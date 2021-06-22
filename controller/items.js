5605bdf13df790a9201373069adcb73367066d42
Anatame <anata.me97@gmail.com>
2021-06-22 15:09:15 +0530

    Added discord setup, the server receives message from the extension and sends it by the bot to a specific channel

--- controller/items.js
+++ controller/items.js
@@ -1,3 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('../config.json');
+
+client.once('ready', () => {
+   console.log('Ready!');
+});
+
+client.login(config.token);
+
 const getItems = (req, reply) => {
    reply.send("All items")
 }
@@ -11,10 +21,10 @@ const getItem = (req, reply) => {
 }
 
 const addItem = (req, reply) => {
-   const name = req.body.name
-   console.log(name)
-
-   reply.send(name)
+   const data = JSON.parse(req.body)
+   console.log(data.name)
+   reply.send(data.name)
+   client.channels.cache.get(`856609332288421898`).send(data.name)
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config.json');
+
 const PORT = 5000;
 
 const fastify = require('fastify')({ logger: true })
@@ -18,15 +16,3 @@ const start = async () => {
 
 start();
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('message', message => {
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
-	}
-});
-
-client.login(config.token);
