3725312e0648c5fd62793fcbb04841c1ab442c5a
Anatame <anata.me97@gmail.com>
2021-06-24 02:44:32 +0530

    Added on guid add message and getting the user id of the user who adds the bot

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-
+const { client } = require('./utils/discord')
 const PORT = 5000;
-
 const fastify = require('fastify')({ logger: true })
+
+client.on("guildCreate", guild => { // This event fires when a guild is created or when the bot is added to a guild.
+	guild.fetchAuditLogs({type: "BOT_ADD", limit: 1}).then(log => { // Fetching 1 entry from the AuditLogs for BOT_ADD.
+		log.entries.first().executor.send(`Thank you for adding me to ${guild.name}!`).catch(e => console.error(e)); // Sending the message to the executor.
+		console.log(log.entries.first().executor.id)
+	});
+});
+
+
+module.exports = {client};
 
 fastify.register(require('./routes/items'))
 
