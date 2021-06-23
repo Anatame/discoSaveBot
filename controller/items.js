e419e17c2cf2050afe7b1115df35297365bce571
Anatame <anata.me97@gmail.com>
2021-06-24 02:28:42 +0530

    Added utils folder and added discord js init file which exports client

--- controller/items.js
+++ controller/items.js
@@ -1,12 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('../config.json');
-
-client.once('ready', () => {
-   console.log('Ready!');
-});
-
-client.login(config.token);
+const {client} = require('../utils/discord.js');
 
 const getItems = (req, reply) => {
    reply.send("All items")
@@ -21,7 +13,7 @@ const getItem = (req, reply) => {
 }
 
 const addItem = (req, reply) => {
-   const data = JSON.parse(req.body)
+   const data = req.body
    console.log(data.name)
    reply.send(data.name)
    client.channels.cache.get(`856609332288421898`).send(data.name)
