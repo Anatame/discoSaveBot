cd56f9e4ef047e93c0b0bfe55bbe1fcd719c43a6
Anatame <anata.me97@gmail.com>
2021-07-22 14:25:57 +0530

    added db url

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set("debug", true);
-mongoose.connect("mongodb://localhost/discoSaveBeta", {
+mongoose.connect(process.env.dbURL || "mongodb://localhost/discoSaveBeta", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
