56714dce7df41e3ee6c8530ba7a7ab5781dc9406
Anatame <anata.me97@gmail.com>
2021-07-22 14:18:33 +0530

    removed local db url

--- database/schemas/MessageConfig.js
+++ database/schemas/MessageConfig.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.dbURL || 'mongodb://localhost:27017/discoSave',  {useUnifiedTopology:true, useNewUrlParser:true});
+mongoose.connect(process.env.dbURL,  {useUnifiedTopology:true, useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
       id: Number,
